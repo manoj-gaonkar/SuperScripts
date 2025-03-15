0339113de04cfbbb6d4c24885ffb11066cc031a0
manoj-gaonkar <manojgaonkar7648@gmail.com>
2025-03-15 23:06:07 +0530

    server connectivity check script

--- check_connectivity.sh
+++ check_connectivity.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # Author: Manoj
+#
+# Crontab used for run is as below
+# ----------------------------------------------------------------------------------
+# 03 23 * * * /home/manoj/learn/scripts/RealWorldScripts/check_connectivity.sh >> /home/manoj/learn/scripts/RealWorldScripts/logs/connectivity_check.log 2>&1
+# ----------------------------------------------------------------------------------
 
 hosts=('10.0.2.15' '192.17.0.1')
 
 for host in "${hosts[@]}";
 do
+	timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 	ping -c 1 "$host" 1>/dev/null 2>&1
 	if [[ $? -eq 0 ]];
 	then
-		echo "${host} Server is running"
+		echo -e "${timestamp} \e[32mOK\e[0m ${host} Server is running"
 	else
-		echo "${host} Server is not running"
+		echo -e "${timestamp} \e[31mNOK\e[0m ${host} Server is not running"
 	fi
 done
 
